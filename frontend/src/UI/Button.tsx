8c688fd451d146f8e655d9279000bf3e37e25f5f
Imthadh Ahamed <143585845+imthadh-ahamed@users.noreply.github.com>
2024-08-14 17:48:59 +0530

    Create reusable Button component

---
+++ frontend/src/UI/Button.tsx
@@ -0,0 +1,26 @@
+import { ButtonHTMLAttributes, FC } from "react";
+import classNames from "classnames";
+
+// Button component props
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string;
+}
+
+// Button component
+export const Button: FC<ButtonProps> = ({ className, children, ...props }) => {
+  return (
+    <button
+      className={classNames(
+        "w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-black hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500",
+        className
+      )}
+      // aria-pressed is set to false by default
+      aria-pressed="false"
+      // Spread the rest of the props
+      {...props}
+    >
+      {/* Render the children */}
+      {children}
+    </button>
+  );
+};
