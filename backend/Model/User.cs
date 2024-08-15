03f65be56c4e91c4db91d045b1bc3474d0525d96
Imthadh Ahamed <143585845+imthadh-ahamed@users.noreply.github.com>
2024-08-15 22:47:08 +0530

    Create User model

---
+++ backend/Model/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Model
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+    }
+}
