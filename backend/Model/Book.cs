adf3b4a9ca715f2104bbd4b0fa56e9d095f8380d
Imthadh Ahamed <143585845+imthadh-ahamed@users.noreply.github.com>
2024-08-15 22:51:25 +0530

    Create Book model

---
+++ backend/Model/Book.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Model
+{
+    public class Book
+    {
+        [Key]
+        public int BookId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
