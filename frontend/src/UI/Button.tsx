b3f3dbc09aac0768f59c0609a28bee03d7b9f9ed
Imthadh Ahamed <143585845+imthadh-ahamed@users.noreply.github.com>
2024-08-17 11:37:48 +0530

    Backend Database updated

--- frontend/src/UI/Button.tsx
+++ frontend/src/UI/Button.tsx
@@ -65,7 +65,7 @@ export const GreenButton: FC<ButtonProps> = ({ className, children, ...props }) 
   return (
     <button
       className={classNames(
-        "w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500",
+        "flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500",
         className
       )}
       // aria-pressed is set to false by default
