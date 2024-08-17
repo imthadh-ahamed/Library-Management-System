72b5fb3099e4cb0fe9bb895efcee25519a4c1004
Imthadh Ahamed <143585845+imthadh-ahamed@users.noreply.github.com>
2024-08-17 12:01:01 +0530

    Update the component index

--- frontend/src/Components/index.tsx
+++ frontend/src/Components/index.tsx
@@ -1,6 +1,6 @@
 import Header from "./Header"
 import Footer from "./Footer";
 import CustomModal from "./Modal";
-import PrivateRoute from "./PrivateRoute";
+import { PrivateRoute } from "./PrivateRoute";
 
 export { Header, Footer, CustomModal, PrivateRoute };
